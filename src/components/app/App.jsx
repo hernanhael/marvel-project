22a532d6fc2fb862c40c8a20f45288f859b30e7d
Hernán Hael <hernan_hael@hotmail.com>
2023-07-11 10:56:06 -0300

    feat: add API request

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,30 @@
 import { Header } from "../header/Header";
+import { useState, useEffect } from "react";
 import "./app.css";
 
-function App() {
+const App = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const API = await fetch("https://swapi.dev/api/people?page=1");
+
+      setCharacters((await API.json()).results);
+    };
+
+    fetchCharacters();
+  }, []);
+
   return (
-    <div className="App">
+    <div>
       <Header />
+      <ul>
+        {characters.map(({ name }) => (
+          <li>{name}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
