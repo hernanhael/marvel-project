c68804df348ff2631f879d85fd6e0f895dd3a410
Patricio Balardini <patriciobalardini@gmail.com>
2023-07-13 22:46:36 +0800

    fix: remplace  app component to main component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,33 +1,13 @@
 import { Header } from "../header/Header";
-import { useState, useEffect } from "react";
+import { Main } from "../main/Main";
 import "./app.css";
 
 const App = () => {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    const fetchCharacters = async () => {
-      try {
-        const API = await fetch("https://swapi.dev/api/people?page=1");
-
-        setCharacters((await API.json()).results);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    fetchCharacters();
-  }, []);
-
   return (
-    <div>
+    <>
       <Header />
-      <ul>
-        {characters.map(({ name }) => (
-          <li>{name}</li>
-        ))}
-      </ul>
-    </div>
+      <Main />
+    </>
   );
 };
 
